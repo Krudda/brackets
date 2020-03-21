c0041f4d7a658d9813094bfcaea7f2bc5e465a97
Dmitriy <krudda@yandex.ru>
2020-03-21 18:11:52 +0300

    feat: brackets first decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let strArr = str.split('');
+    let result = false;
+    
+    // console.log(strArr);
+
+    if (strArr.length % 2 != 0 ) return result;
+
+    // console.log(strArr);
+
+    strArr.forEach((bracket, index, strArr) => {
+        // console.log('Текущая скобка равна: ', bracket);
+        // console.log('.. а индекс равен: ', index);
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if ( bracket ==  bracketsConfig[i][1] ) {
+                // console.log('Попалась закрывашка! ');
+                if ( strArr[index - 1] == bracketsConfig[i][0] ) {
+                    // console.log('Правильная закрывашка! ');
+                    result = true;
+                    strArr.splice([index - 1], 1, 'удаленная открвывающая скобка');
+                    strArr.splice([index], 1, 'удаленная закрывающая скобка');
+                    // console.log(strArr);
+                }
+                else {
+                    // console.log('Не правильная закрывашка!');
+                    return result = false;
+                }
+            }
+        }
+    });
+    return result;
 }
