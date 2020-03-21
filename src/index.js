765f518b69720d29e0b8116d4fbb0f661c546493
Dmitriy <krudda@yandex.ru>
2020-03-21 18:48:51 +0300

    feat: second attempt to solve

--- src/index.js
+++ src/index.js
@@ -2,32 +2,40 @@ module.exports = function check(str, bracketsConfig) {
 
     let strArr = str.split('');
     let result = false;
-    
-    // console.log(strArr);
 
     if (strArr.length % 2 != 0 ) return result;
 
-    // console.log(strArr);
-
-    strArr.forEach((bracket, index, strArr) => {
-        // console.log('Текущая скобка равна: ', bracket);
-        // console.log('.. а индекс равен: ', index);
+    strArr.forEach((bracket, index) => {
         for (let i = 0; i < bracketsConfig.length; i++) {
             if ( bracket ==  bracketsConfig[i][1] ) {
-                // console.log('Попалась закрывашка! ');
                 if ( strArr[index - 1] == bracketsConfig[i][0] ) {
-                    // console.log('Правильная закрывашка! ');
                     result = true;
-                    strArr.splice([index - 1], 1, 'удаленная открвывающая скобка');
-                    strArr.splice([index], 1, 'удаленная закрывающая скобка');
-                    // console.log(strArr);
+                    strArr.splice([index - 1], 1, 'DEL');
+                    strArr.splice([index], 1, 'DEL');
                 }
+                else if ( strArr[index - 1] == 'DEL') {
+                    if (strArr[index - 3] == bracketsConfig[i][0]) {
+                        result = true;
+                        strArr.splice([index - 3], 1, 'DEL');
+                        strArr.splice([index], 1, 'DEL');
+                    }
+                    else if (strArr[index - 3] == 'DEL') {
+                        if (strArr[index - 5] == bracketsConfig[i][0]) {
+                            result = true;
+                            strArr.splice([index - 5], 1, 'DEL');
+                            strArr.splice([index], 1, 'DEL');
+                            console.log(strArr);
+                        }
+                    }
+                else return result = false;
+                } 
                 else {
-                    // console.log('Не правильная закрывашка!');
                     return result = false;
                 }
             }
         }
     });
-    return result;
+    let resArr = strArr.filter(el => el !== 'DEL');
+    if (resArr.length > 0) return false;
+    else return true;
 }
